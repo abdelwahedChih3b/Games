66c3bc1b674f2b1f77984f1616257fafd923b149
chiheb <abdelwahedchiheb0@gmail.com>
2021-12-14 17:27:01 +0100

    main

--- Laybrinth.py
+++ Laybrinth.py
@@ -23,31 +23,35 @@ def path( mat) :
     i,j =1,1
     iPlus =0
     jPlus =0
-    while i != 10 and j !=10 :
-        
-        if mat[i+1][j] == 1 :
-            iPlus = 1
-            jPlus = 0
-            i+=1
-        elif mat[i][j+1] == 1 :
-            iPlus = 0
-            jPlus = 1
-        elif mat[i-1][j] == 1 :
-            iPlus = -1
-            jPlus = 0
-        elif mat[i][j-1] == 1 :
-            iPlus = 0
-            jPlus = -1
-        else : print("blocked")
-        j += jPlus
-        i += iPlus
-        if mat[i+1][j] + mat[i][j+1] + mat[i-1][j] + mat[i][j-1] == 3 : 
+    direct = []
+    while i != 10 and j !=10 and i <=11 and j<= 12 :
+        if mat[i+1][j] + mat[i][j+1] + mat[i-1][j] + mat[i][j-1] == 4 : 
             mat[i][j] = 0
             i -= iPlus
             j -= jPlus
+            
+        if mat[i+1][j] == 0 :
+            iPlus = 1
+            jPlus = 0
+            direct.append("E")
+        elif mat[i][j+1] ==0 :
+            iPlus = 0
+            jPlus = 1
+            direct.append("S")
+        elif mat[i-1][j] == 0 :
+            iPlus = -1
+            jPlus = 0
+            direct.append("W")
+        elif mat[i][j-1] == 0 :
+            iPlus = 0
+            jPlus = -1
+            direct.append("N")
+        j = j+ jPlus
+        i = i+ iPlus
+        
         print("i = {} , j = {}".format(i,j))
-        time.sleep(100)
-    print("done")
+        
+    print(direct)
 
 path(mat)
 
